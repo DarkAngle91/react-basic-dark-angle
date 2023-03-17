a69e1d689c5621f35e9bb1578d8947858765326a
DarkAngle91 <82767929+DarkAngle91@users.noreply.github.com>
2023-03-17 12:13:41 +0700

    state tutorial

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+
+    // key: value
+    state = {
+        name: 'Ronaldo',
+        channel: 'Hung JP channel',
+
+    }
+
+
+    /**
+     * // Cú pháp bọc đặc biệt <React.Fragment></React.Fragment> 
+     */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
         return (
-            <div>Hello my component Hung JP</div>
+            <React.Fragment>
+
+                <div className="fisrt">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => { this.handleOnChangeName(event) }}
+                    />
+                    My name's {this.state.name}
+                </div>
+                <div className="second">
+                    My youtube channel: {this.state.channel}
+                </div>
+            </React.Fragment>
         )
     }
 }
