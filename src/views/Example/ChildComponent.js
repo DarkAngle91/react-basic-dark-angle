aee07c1fc0839bc6743b61325654f4a730725965
DarkAngle91 <82767929+DarkAngle91@users.noreply.github.com>
2023-03-18 21:42:35 +0700

    13 stateless/statefull

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,59 +1,51 @@
 import React from 'react';
 
-class ChildComponent extends React.Component {
+// class component
+// class ChildComponent extends React.Component {
+//     // re-render
+//     render() {
+//         console.log('check props ', this.props)
+//         // let name = this.props.name
+//         // let age = this.props.age
+
+//         let { name, age, address, arrJobs } = this.props // Cú pháp khai báo biến ngắn lại
+//         return (
+//             <>
+//                 <div className="job-list">
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
 
 
-    // key: value
-    state = {
-        firstName: '',
-        lastName: ''
-    }
-
-    handleChangeFisrtName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
-
-    handeChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault() // Function ngăn load lại trang khi bấm submit
-        alert('Click me to submit')
-        console.log('Check data input: ', this.state) // Kiểm tra xem data đã vào chưa
-    }
-    /**
-     * // Cú pháp bọc đặc biệt <React.Fragment></React.Fragment> 
-     */
-
-    // re-render
-    render() {
-        console.log('check props ', this.props)
-        // let name = this.props.name
-        // let age = this.props.age
-
-        let { name, age, address, arrJobs } = this.props // Cú pháp khai báo biến ngắn lại
-        let a = '';
-        return (
-            <>
-                <div className="job-list">
-                    {
-                        a = arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title} - {item.salary}
-                                </div>
-                            )
-                        })
-                    }
-                    {console.log('Check map array:', a)}
-                </div>
-            </>
-        )
-    }
+// function component
+const ChildComponent = (props) => {
+    let { arrJobs } = props // Cú pháp khai báo biến ngắn lại
+    return (
+        <>
+            <div className="job-list">
+                {
+                    arrJobs.map((item, index) => {
+                        return (
+                            <div key={item.id}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
 
 export default ChildComponent;
