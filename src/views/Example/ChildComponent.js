73dfe87158a30708fb02fcd5c03b11481fcf81a8
DarkAngle91 <82767929+DarkAngle91@users.noreply.github.com>
2023-03-20 10:17:29 +0700

    #15 Split Components

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,51 +1,76 @@
 import React from 'react';
 
 // class component
-// class ChildComponent extends React.Component {
-//     // re-render
-//     render() {
-//         console.log('check props ', this.props)
-//         // let name = this.props.name
-//         // let age = this.props.age
+class ChildComponent extends React.Component {
+    // re-render
 
-//         let { name, age, address, arrJobs } = this.props // Cú pháp khai báo biến ngắn lại
-//         return (
-//             <>
-//                 <div className="job-list">
-//                     {
-//                         arrJobs.map((item, index) => {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title} - {item.salary}
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//             </>
-//         )
-//     }
-// }
+    //khai baos state
+    state = {
+        showJob: false
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            showJob: !this.state.showJob
+        })
+    }
+    render() {
+
+        let { arrJobs } = this.props // Cú pháp khai báo biến ngắn lại
+        let { showJob } = this.state
+        let check = showJob === true ? 'showJob = true' : 'showJob = false';
+        console.log('>>>check condition: ', check)
+        return (
+            <>
+                {showJob === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    <>
+                        <div className="job-lists">
+                            {
+                                arrJobs.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                }
+            </>
+        )
+    }
+}
 
 
 // function component
-const ChildComponent = (props) => {
-    let { arrJobs } = props // Cú pháp khai báo biến ngắn lại
-    return (
-        <>
-            <div className="job-list">
-                {
-                    arrJobs.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {item.title} - {item.salary}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
-}
+// const ChildComponent = (props) => {
+//     let { arrJobs } = props // Cú pháp khai báo biến ngắn lại
+//     return (
+//         <>
+//             <div className="job-list">
+//                 {
+//                     arrJobs.map((item, index) => {
+//                         if (item.salary >= 500) {
+
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary} $
+//                                 </div>
+//                             )
+//                         }
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
+// }
 
 export default ChildComponent;
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import ChildComponent from './ChildComponent';
+import AddComponent from './AddComponent';
 
 class MyComponent extends React.Component {
 
 
     // key: value
     state = {
-        firstName: '',
-        lastName: '',
         arrJobs: [
-            { id: 'abcJob1', title: 'developer', salary: '500$' },
-            { id: 'abcJob2', title: 'Tester', salary: '400$' },
-            { id: 'abcJob3', title: 'Project Manager', salary: '1000$' },
+            { id: 'abcJob1', title: 'developer', salary: '500' },
+            { id: 'abcJob2', title: 'Tester', salary: '400' },
+            { id: 'abcJob3', title: 'Project Manager', salary: '1000' },
         ]
     }
 
-    handleChangeFisrtName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
 
-    handeChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault() // Function ngăn load lại trang khi bấm submit
-        alert('Click me to submit')
-        console.log('Check data input: ', this.state) // Kiểm tra xem data đã vào chưa
-    }
+
     /**
      * // Cú pháp bọc đặc biệt <React.Fragment></React.Fragment> 
      */
@@ -40,30 +25,8 @@ class MyComponent extends React.Component {
         console.log('call render: ', this.state)
         return (
             <>
-                <form >
-                    <label htmlFor="fname">First name:</label><br />
-                    <input
-                        type="text"
-                        value={this.state.firstName}
-                        onChange={(event) => this.handleChangeFisrtName(event)}
-                    />
-                    <br />
-                    <label htmlFor="lname">Last name:</label><br />
-                    <input
-                        type="text"
-                        value={this.state.lastName}
-                        onChange={(event) => this.handeChangeLastName(event)}
-                    /><br /><br />
-                    <input
-                        type="submit" value="Submit"
-                        onClick={(event) => this.handleSubmit(event)}
-                    />
-                </form>
-
+                <AddComponent />
                 <ChildComponent
-                    name={this.state.firstName}
-                    age={'25'}
-                    address={'Kien Giang'}
                     arrJobs={this.state.arrJobs}
                 />
             </>
